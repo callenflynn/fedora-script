a093628a0d6a9934d680cf236af1d8a0517f49ab
Callen Flynn <cal@callen.page>
2026-08-13 23:15:59 -0400

    Fix Fedora 44 GNOME installation and use preselected options

--- gnome.sh
+++ gnome.sh
@@ -5,12 +5,22 @@ source "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)/common.sh"
 require_user
 
 log "Installing GNOME desktop"
-sudo dnf group install -y "GNOME Desktop Environment"
+sudo dnf install -y @gnome-desktop
 
 install_apps
-ask_gaming
-ask_icloud_sync
-ask_proton_pass
+
+if [[ "${INSTALL_GAMING:-0}" == "1" ]]; then
+    install_gaming_apps
+fi
+
+if [[ "${INSTALL_ICLOUD:-0}" == "1" ]]; then
+    install_icloud_sync
+fi
+
+if [[ "${INSTALL_PROTON_PASS:-0}" == "1" ]]; then
+    install_proton_pass
+fi
+
 install_cursor
 install_wallpapers
 
