e0fd55c4349baa85adad4f8e4db24e1cdeb7b699
Callen Flynn <cal@callen.page>
2026-08-13 23:03:24 -0400

    Fix all interactive prompts for piped execution

--- common.sh
+++ common.sh
@@ -15,7 +15,7 @@ require_user() {
 ask_yes_no() {
     local prompt="$1" answer
     while true; do
-        read -r -p "$prompt [y/N] " answer
+        read -r -p "$prompt [y/N] " answer </dev/tty
         case "${answer,,}" in
             y|yes) return 0 ;;
             n|no|"") return 1 ;;
